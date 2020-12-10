d9c92b7dce2ef8c7410f3c98d35ab141137f17b2
JE-Chen <zenmailman@gmail.com>
2020-12-10 11:59:56 +0800

    add UnitTest and refactor
    add UnitTest and refactor

--- src/main/java/Module/Generate_VerificationCode.java
+++ src/main/java/Module/Generate_VerificationCode.java
@@ -14,29 +14,29 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Generate_VerificationCode {
 
-    public List<Integer> Generate_Color(){
+    public List<Integer> Generate_Color() {
         List<Integer> RGB = new ArrayList<>();
-        int R = ThreadLocalRandom.current().nextInt(0,255 + 1);
-        int G = ThreadLocalRandom.current().nextInt(0,255 + 1);
-        int B = ThreadLocalRandom.current().nextInt(0,255 + 1);
+        int R = ThreadLocalRandom.current().nextInt(0, 255 + 1);
+        int G = ThreadLocalRandom.current().nextInt(0, 255 + 1);
+        int B = ThreadLocalRandom.current().nextInt(0, 255 + 1);
         RGB.add(R);
         RGB.add(G);
         RGB.add(B);
         return RGB;
     }
 
-    public List<List> Generate_Code(int Max_Num,int Max_Alpha){
+    public List<List> Generate_Code(int Max_Num, int Max_Alpha) {
         assert Max_Num > 0;
         assert Max_Alpha > 0;
         List<Integer> Num = new ArrayList<>();
         List<String> LowAlpha = new ArrayList<>();
         List<List> Random_List = new ArrayList<>();
-        for(int num_list=0;num_list<Max_Num;num_list++) {
-            int Random_Num = ThreadLocalRandom.current().nextInt(0,9 + 1);
+        for (int num_list = 0; num_list < Max_Num; num_list++) {
+            int Random_Num = ThreadLocalRandom.current().nextInt(0, 9 + 1);
             Num.add(Random_Num);
         }
-        for(int alpha_list=0;alpha_list<Max_Alpha;alpha_list++) {
-            String Random_Alpha = Character.toString((char)ThreadLocalRandom.current().nextInt(97,122 + 1));
+        for (int alpha_list = 0; alpha_list < Max_Alpha; alpha_list++) {
+            String Random_Alpha = Character.toString((char) ThreadLocalRandom.current().nextInt(97, 122 + 1));
             LowAlpha.add(Random_Alpha);
         }
         Random_List.add(Num);
@@ -44,17 +44,17 @@ public class Generate_VerificationCode {
         return Random_List;
     }
 
-    public List<String> Generate_Code(int Max_Alpha){
+    public List<String> Generate_Code(int Max_Alpha) {
         assert Max_Alpha > 0;
         List<String> LowAlpha = new ArrayList<>();
-        for(int alpha_list=0;alpha_list<Max_Alpha;alpha_list++) {
-            String Random_Alpha = Character.toString((char)ThreadLocalRandom.current().nextInt(97,122 + 1));
+        for (int alpha_list = 0; alpha_list < Max_Alpha; alpha_list++) {
+            String Random_Alpha = Character.toString((char) ThreadLocalRandom.current().nextInt(97, 122 + 1));
             LowAlpha.add(Random_Alpha);
         }
         return LowAlpha;
     }
 
-    public BufferedImage Generate_Image(int Height,int Width,int LineCount,String DrawString,int FontSize,String File_Name,boolean Save){
+    public BufferedImage Generate_Image(int Height, int Width, int LineCount, String DrawString, int FontSize, String File_Name, boolean Save) {
         assert Height > 0;
         assert Width > 0;
         assert LineCount > 0;
@@ -67,27 +67,27 @@ public class Generate_VerificationCode {
         Color LineColor = new Color(LineRGB.get(0), LineRGB.get(1), LineRGB.get(2));
 
         Color BackgroundColor = new Color(BackgroundRGB.get(0), BackgroundRGB.get(1), BackgroundRGB.get(2));
-        BufferedImage CodeImage = new BufferedImage(Width,Height,BufferedImage.TYPE_INT_RGB);
+        BufferedImage CodeImage = new BufferedImage(Width, Height, BufferedImage.TYPE_INT_RGB);
 
         Graphics Painter = CodeImage.getGraphics();
         Painter.setColor(BackgroundColor);
-        Painter.fillRect(0,0,Width,Height);
+        Painter.fillRect(0, 0, Width, Height);
         Painter.setColor(LineColor);
 
-        for(int pre_draw_count=0;pre_draw_count<LineCount;pre_draw_count++)
-            Painter.drawLine(ThreadLocalRandom.current().nextInt(Width),0,ThreadLocalRandom.current().nextInt(Width),Height);
+        for (int pre_draw_count = 0; pre_draw_count < LineCount; pre_draw_count++)
+            Painter.drawLine(ThreadLocalRandom.current().nextInt(Width), 0, ThreadLocalRandom.current().nextInt(Width), Height);
 
         Painter.setColor(FontColor);
-        Painter.setFont(new Font("TimesRoman",Font.BOLD,FontSize));
-        Painter.drawString(DrawString,FontSize,(Height+(FontSize/2))/2);
+        Painter.setFont(new Font("TimesRoman", Font.BOLD, FontSize));
+        Painter.drawString(DrawString, FontSize, (Height + (FontSize / 2)) / 2);
 
         Painter.setColor(LineColor);
-        for(int after_draw_count=0;after_draw_count<LineCount;after_draw_count++)
-            Painter.drawLine(0,ThreadLocalRandom.current().nextInt(Height),Width,ThreadLocalRandom.current().nextInt(Height));
+        for (int after_draw_count = 0; after_draw_count < LineCount; after_draw_count++)
+            Painter.drawLine(0, ThreadLocalRandom.current().nextInt(Height), Width, ThreadLocalRandom.current().nextInt(Height));
 
         Painter.dispose();
 
-        if(Save) {
+        if (Save) {
             try {
                 ImageIO.write(CodeImage, "png", new File(File_Name));
             } catch (IOException e) {
@@ -97,21 +97,21 @@ public class Generate_VerificationCode {
         return CodeImage;
     }
 
-    public String Image_To_Base64String(BufferedImage Image,String Type){
+    public String Image_To_Base64String(BufferedImage Image, String Type) {
         String Imagebase64 = null;
         ByteArrayOutputStream ImageByteStream = new ByteArrayOutputStream();
         try {
-            ImageIO.write(Image,Type,ImageByteStream);
+            ImageIO.write(Image, Type, ImageByteStream);
             byte[] ImageByte = ImageByteStream.toByteArray();
             Imagebase64 = Base64.getEncoder().encodeToString(ImageByte);
             ImageByteStream.close();
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return Imagebase64;
     }
 
-    public BufferedImage Base64String_To_Image(String Base64String){
+    public BufferedImage Base64String_To_Image(String Base64String) {
         BufferedImage ImageBase64 = null;
         byte[] ImageByte;
         try {
@@ -119,7 +119,7 @@ public class Generate_VerificationCode {
             ByteArrayInputStream ImageByteStream = new ByteArrayInputStream(ImageByte);
             ImageBase64 = ImageIO.read(ImageByteStream);
             ImageByteStream.close();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return ImageBase64;
---
+++ src/test/java/Module/Generate_VerificationCodeTest.java
@@ -0,0 +1,86 @@
+package Module;
+
+import org.junit.jupiter.api.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+class Generate_VerificationCodeTest {
+
+    Generate_VerificationCode Code = new Generate_VerificationCode();
+
+    @Test
+    void generate_Color() {
+        for (Integer i : Code.Generate_Color()) {
+            System.out.println(i);
+        }
+        System.out.println();
+    }
+
+    @Test
+    void generate_Code() {
+        for (String i : Code.Generate_Code(5)) {
+            System.out.print(" " + i);
+        }
+        System.out.println();
+    }
+
+    @Test
+    void generate_Image() {
+        StringBuilder CodeBuilder = new StringBuilder();
+        for (String i : Code.Generate_Code(5)) {
+            CodeBuilder.append(i);
+        }
+        System.out.println();
+
+        String AlphaCode = CodeBuilder.toString();
+        System.out.println(AlphaCode);
+        BufferedImage BFImage = Code.Generate_Image(75, 200, 5, AlphaCode, 40, "CodeImage.png", true);
+
+        System.out.println();
+    }
+
+    @Test
+    void image_To_Base64String() {
+        StringBuilder CodeBuilder = new StringBuilder();
+        for (String i : Code.Generate_Code(5)) {
+            CodeBuilder.append(i);
+        }
+        System.out.println();
+
+        String AlphaCode = CodeBuilder.toString();
+        System.out.println(AlphaCode);
+        BufferedImage BFImage = Code.Generate_Image(75, 200, 5, AlphaCode, 40, "CodeImage.png", true);
+
+        System.out.println();
+        String Base64String = Code.Image_To_Base64String(BFImage, "png");
+        System.out.println(Base64String);
+
+    }
+
+    @Test
+    void base64String_To_Image() {
+        StringBuilder CodeBuilder = new StringBuilder();
+        for (String i : Code.Generate_Code(5)) {
+            CodeBuilder.append(i);
+        }
+        System.out.println();
+
+        String AlphaCode = CodeBuilder.toString();
+        System.out.println(AlphaCode);
+        BufferedImage BFImage = Code.Generate_Image(75, 200, 5, AlphaCode, 40, "CodeImage.png", true);
+
+        System.out.println();
+        String Base64String = Code.Image_To_Base64String(BFImage, "png");
+        System.out.println(Base64String);
+        System.out.println();
+        BufferedImage Base64Image = Code.Base64String_To_Image(Base64String);
+        try {
+            ImageIO.write(Base64Image, "png", new File("Base64 TO Image.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
